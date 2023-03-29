ac9b60dbb78320c66fe6716e5a55566a1368a0c1
abdu95873 <abdu95873@gmail.com>
2023-03-29 13:33:07 +0600

    first commit

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import './Product.css'
 
 const Product = (props) => {
     const {img, name, seller, quantity, price, ratings}=props.product
+    const handleAddToCart = props.handleAddToCart
+
+
+    
+
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +22,7 @@ const Product = (props) => {
             <p>Menufacturer: {seller}</p>
             <p>Rating: {ratings} Star</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('products.json')
@@ -12,6 +13,11 @@ const Shop = () => {
 
     }, [])
 
+    const handleAddToCart = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -19,12 +25,15 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart = {handleAddToCart}
                     ></Product>)
                 }
 
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+
             </div>
         </div>
     );
